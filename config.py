685dc631e875929031581623450ff6c591b70d61
MRoyhanF <froyhan0@gmail.com>
2024-01-21 10:52:22 +0700

    setup sql

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    HOST = str(os.environ.get("DB_HOST"))
+    DATABASE = str(os.environ.get("DB_DATABASE"))
+    USERNAME = str(os.environ.get("DB_USERNAME"))
+    PASSWORD = str(os.environ.get("DB_PASSWORD"))
+
+    JWT_SECRET_KEY = str(os.environ.get("JWT_SECRET"))
+    
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
+    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_RECORD_QUERIES = True
