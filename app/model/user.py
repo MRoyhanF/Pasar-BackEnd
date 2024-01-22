8b629020390a43d13700018201775e8d89768b57
MRoyhanF <froyhan0@gmail.com>
2024-01-22 10:14:02 +0700

    fix a database

--- app/model/user.py
+++ app/model/user.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class User(db.Model):
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     name = db.Column(db.String(250), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
     email = db.Column(db.String(60), index=True, unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
     create_at = db.Column(db.DateTime, default=datetime.utcnow)
